df9c95c512ac5d418fb6811e485afbad598f8898
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-17 11:59:07 +0530

    not operator
    practiced logical operator

---
+++ nearest even number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter the number:");
+    scanf("%d", &a);
+    if (a % 2 == 0)
+    {
+        printf("the nearest even no is:%d", a);
+    }
+    else
+    {
+        printf("the nearest even no is:%d", a);
+    }
+    return 0;
+}
