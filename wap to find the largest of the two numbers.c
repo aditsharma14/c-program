4ef1566ec7e2f0e43696c9038fd0d0e5335fd1f0
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-28 11:58:35 +0530

    wap to find the largest of two numbers.
    wap to find the largest of two numbers.

---
+++ wap to find the largest of the two numbers.c
@@ -0,0 +1,16 @@
+// wap to find the largest of two numbers.
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    if (a > b)
+    {
+        printf("first number is greater than the second");
+    }
+    else
+    {
+        printf("second one is greater than the first one");
+    }
+}
