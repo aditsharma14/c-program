95309dfabaef9600bef0aa7509223c81021e4224
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-31 12:53:00 +0530

    program to print numbers from 1 to 56
     program to print numbers from 1 to 56 by using while loop

---
+++ 1 to n while loop.c
@@ -0,0 +1,13 @@
+// program to print numbers from 1 to 56
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i <= 56)
+    {
+        printf("%d", i);
+        printf("\n");
+        i = i + 1;
+    }
+    return 0;
+}
