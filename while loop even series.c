25871e5f1fb07c2ea6146e01419fc8daf4d287c2
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-31 12:27:09 +0530

    program to print even series within 50
    used while loop to print even numbers within 0 to 50

---
+++ while loop even series.c
@@ -0,0 +1,13 @@
+// program to print even series within 50
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    while (i <= 50)
+    {
+        printf("%d", i);
+        printf("\n");
+        i = i + 2;
+    }
+    return 0;
+}
